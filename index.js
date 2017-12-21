2eeed769daa189b955bb639f0d268330205c254a
Jane Ossai <buff_jane@hotmail.com>
2017-12-21 16:57:10 +0000

    restructure

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+angular.module('app').controller("MainController", function($scope, $rootScope, $location) {
+
+
+  $scope.$on('$viewContentLoaded', function() {
+      console.log("starting firebase");
+      startFirebase();
+  });
+
 var url = "https://sofo.mediasite.com/Mediasite/Play/d64d7806bcc14f95a3c57633bcfd30c31d";
 var player;
 var controls;
@@ -5,7 +13,9 @@ var transcriptKey;
 var editor;
 var currentFirepad;
 var currentID;
-var noUsers = 0;
+var playKeyDown = false;
+var playing = false;
+var noUsers;
 
 function startFirebase() {
   var config = {
@@ -17,8 +27,15 @@ function startFirebase() {
     messagingSenderId: "807025189838"
   };
 
-  firebase.initializeApp(config);
-
+  try {
+      firebase.initializeApp(config);
+    } catch (err) {
+      // we skip the "already exists" message which is
+      // not an actual error when we're hot-reloading
+      if (!/already exists/.test(err.message)) {
+        console.error('Firebase initialization error', err.stack)
+      }
+    }
   var uiConfig = {
     signInSuccessUrl: '/',
     signInOptions: [
@@ -28,9 +45,10 @@ function startFirebase() {
   };
 
   //change callback here
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  ui.start('#auth-container', uiConfig);
-  initializeCodeMirror();
+  //var ui = new firebaseui.auth.AuthUI(firebase.auth());
+  //ui.start('#auth-container', uiConfig);
+  //ui.delete(); //delete ui or it will cause an error
+  //initializeCodeMirror();
   getPlayerReference(startDatabase);
 }
 
@@ -39,11 +57,13 @@ function initializeCodeMirror() {
 }
 
 function getPlayerReference(callback) {
+  console.log("Getting player reference");
   player = new Mediasite.Player( "player",
     {
       url: url + (url.indexOf("?") == -1 ? "?" : "&") + "player=MediasiteIntegration",
       events: {
-                    "ready":   callback
+                    "ready":   callback,
+                    "playstatechanged": updatePlayingState
               }
     });
 
@@ -55,18 +75,21 @@ function getPlayerReference(callback) {
 
 function startDatabase() {
   var hash = window.location.hash.replace(/#/g, '');
-
-  if(hash) {
+  console.log("hash: " + hash);
+  //angular router has hash in url  already so add this check instaed
+  if(hash !== "/") {
     var ref = firebase.database().ref("/captions/");
-    checkExistence(ref.child(hash)).then(function(exists) {
+    checkExistence(ref.child(hash.replace(/\//g, ""))).then(function(exists) {
       if (exists) {
-        transcriptKey = hash;
+        transcriptKey = hash.replace(/\//g, "");
         loadScript();
       }
 
       else {
+        conole.log("in first else");
         transcriptKey = ref.push().key;
-        window.location = window.location + '#' + transcriptKey;
+        window.location.hash = window.location.hash + '#' + transcriptKey;
+        //console.log("new window location: " + window.location.split('#')[0] + '#' + transcriptKey);
         initTranscript();
       }
     });
@@ -74,7 +97,10 @@ function startDatabase() {
 
   else {
     transcriptKey = firebase.database().ref("/captions/").push().key;
-    window.location = window.location + '#' + transcriptKey;
+    window.location.hash = window.location.hash + "#" + transcriptKey;
+    //console.log("hash is: " + window.location.hash);
+    //history.replaceState(undefined, undefined, '#' + transcriptKey);
+    //console.log("hash is " + window.location.hash);
     initTranscript();
   }
 }
@@ -86,50 +112,24 @@ function checkExistence(dataRef) {
   });
 }
 
-function initializeTranscript() {
-  var captions = player.getCaptions();
-  //captions = captions.slice(0,5);
 
-  var promise = null;
-
-  for(var i = 0; i < captions.length; i++) {
-    var caption = captions[i];
-    if (promise == null) {
-      promise = firebase.database().ref("/captions/" + transcriptKey).push();
-    }
-
-    else {
-      promise = promise.then(function() {
-
-      });
-    }
-  }
-
-  loadTranscript();
-}
-
-function loadTranscript() {
-  listenForCaptions();
-  listenForFirepads();
-  listenToPlayTime();
-}
-
-//TODO: Ensure that element has been added to DOM, if getElementByID is null, maybe
-//try the next best option and whether an element is loaded in check if it is a better option?
-//or add a time attribute to each dom element and search those instead??
 function scrollToSection(key) {
-  var firepadContainer = document.getElementById("firepad-container");
-  var textBox = document.getElementById(key);
-
-  textBox = document.getElementById(key);
+  //var firepadContainer = document.getElementById("firepad-container");
+  //var textBox = document.getElementById(key);
+  console.log("scrolling");
+  //textBox = document.getElementById(key);
   //console.log("Caption key we are looking for: " + key);
   //console.log("Element found for scrolling: " + textBox);
-  var offset = textBox.offsetTop;
-  firepadContainer.scrollTop = offset;
+  //var offset = textBox.offsetTop;
+  //firepadContainer.scrollTop = offset;
+
+  $('#firepad-container').scrollTop(
+    $('#'+key).offset().top - $('#firepad-container').offset().top + $('#firepad-container').scrollTop()
+  );
 }
 
 function initTranscript() {
-
+  console.log("init transcript called");
   //Returns a promise
   var createCaption = function(caption) {
     //console.log("in creating caption");
@@ -150,42 +150,13 @@ function initTranscript() {
     });
   };
 
-  //Need to make a promise here but how??
-  /*var makeElements = function(values) {
-    //console.log("making element");
-    var container = document.getElementById("firepad-container");
-    var divElement = document.createElement('div');
-    var spanElement = document.createElement('span');
-
-    //console.log("made elements");
-
-    divElement.id = values.captionKey;
-    divElement.setAttribute("timestamp", values.time);
-    divElement.classList.add("section");
-    container.appendChild(divElement);
-    divElement.appendChild(spanElement);
-
-    //console.log("Added html element");
-
-    //error occurs here....
-    firebase.database().ref("/firepads/" + transcriptKey + "/" + values.captionKey + "/history").on(
-      "child_added", function(history) {
-        console.log("caption key " +  values.captionKey);
-        var headless = Firepad.Headless(firebase.database().ref("/firepads/" + transcriptKey + "/" + values.captionKey));
-        headless.getText(function(text) {
-          console.log("In callback");
-          spanElement.innerHTML = text;
-        });
-      });
-  };*/
-
   var addFireRef = function(values) {
     //console.log("Adding firepad reference");
     var ref = firebase.database().ref("/firepads/" + transcriptKey + "/" + values.captionKey);
     var headless = Firepad.Headless(ref);
     headless.setText(values.original, function(err, committed) {
       headless.dispose();
-      makeElements(values);
+      //makeElements(values);
     });
   };
 
@@ -201,31 +172,12 @@ function initTranscript() {
     promiseArr.push(promise);
   }
 
-  /*var timeChangedListener = function() {
-    player.addHandler("currenttimechanged", function(event) {
-      var id;
-      firebase.database().ref("/captions/" + transcriptKey).orderByChild("time").endAt(event.currentTime).limitToLast(1).once("value")
-        .then(function(snapshot) {
-          for (keys in snapshot.val()) {
-            id = keys;
-          }
-          if(id != null && id != currentID) {
-            //Need to set current Firepad in here
-            scrollToSection(id);
-          }
-        });
-    });
-  };*/
-
   Promise.all(promiseArr)
-  .then(timeChangedListener)
   .then(setUpCounter)
-  .then(addTranscriptUser)
-  .then(addTranscriptListener);
 }
 
 function loadScript() {
-
+  console.log("Load script called");
   firebase.database().ref("/captions/" + transcriptKey).once("value")
     .then(function (snapshot) {
       return snapshot.val();
@@ -242,35 +194,15 @@ function loadScript() {
     })
     .then(timeChangedListener)
     .then(addTranscriptUser)
-    .then(addTranscriptListener);
+    .then(addTranscriptListener)
+    .then(addDocumentListeners);
 }
 
 function makeElements(values) {
   //console.log("making element");
-  var container = document.getElementById("firepad-container");
-  var divElement = document.createElement('div');
-  var spanElement = document.createElement('span');
-
-  //console.log("made elements");
-
-  divElement.id = values.captionKey;
-  divElement.setAttribute("timestamp", values.time);
-  divElement.classList.add("section");
-  container.appendChild(divElement);
-  divElement.appendChild(spanElement);
-
-  //console.log("Added html element");
-
-  //error occurs here....
-  firebase.database().ref("/firepads/" + transcriptKey + "/" + values.captionKey + "/history").on(
-    "child_added", function(history) {
-      //console.log("caption key " +  values.captionKey);
-      var headless = Firepad.Headless(firebase.database().ref("/firepads/" + transcriptKey + "/" + values.captionKey));
-      headless.getText(function(text) {
-        //console.log("In callback");
-        spanElement.innerHTML = text;
-      });
-    });
+  var container = $("#firepad-container");
+  var sectionElement = createSectionElement(values);
+  container.append(sectionElement);
 }
 
 function timeChangedListener() {
@@ -291,9 +223,9 @@ function timeChangedListener() {
             var cmelement = document.getElementsByClassName('CodeMirror')[0];
             var ccelement = document.getElementById("current-caption");
             ccelement.removeChild(cmelement);
-            initializeCodeMirror();
           }
 
+          initializeCodeMirror();
           currentFirepad = Firepad.fromCodeMirror(firepadRef, editor, {richTextToolbar:false, richTextShortcuts:false});
           currentID = id;
           scrollToSection(id);
@@ -317,11 +249,12 @@ function setUpCounter() {
 
 function addTranscriptUser() {
   console.log("Adding transcript user");
-  //move this around!!
-  var ref = firebase.database().ref("/transcripts/" + transcriptKey + "/users").push({
+
+  var ref = firebase.database().ref("/transcripts/" + transcriptKey + "/users").push();
+  ref.onDisconnect().remove();
+  ref.set({
     id: "placeholder"
   });
-  ref.onDisconnect().remove();
 
 
   //Local test
@@ -341,5 +274,298 @@ function addTranscriptListener() {
   //firebase.database().ref("/transcripts" + transcriptKey + "/dummy").remove();
   ref.on("value", function(count) {
     console.log("Current number of users: " + count.val());
+    noUsers = count.val();
+    $('.collabno').text(noUsers);
   });
 }
+
+
+/**
+ * Given a section object create the section header element
+ * @param section A section object
+ */
+function createSectionHeaderElement(values) {
+    var sectionHeaderElement = $('<div class="section-header"></div>');
+    var timestamp = createSectionTimeStampElement(values.time)
+    sectionHeaderElement.append(timestamp);
+    //sectionHeaderElement.append(createSectionSpeakerElement(section)); - NOT VALUD
+    return sectionHeaderElement;
+    //return timestamp;
+}
+
+function formatStartTime(startTime) {
+    return secondsToHms(startTime);
+}
+
+/**
+ * Create the start time label
+ * @param section a section object
+ */
+function createSectionTimeStampElement(time) {
+    //var sectionTimestampElement = $('<input type="text" size="12" maxlength="16" class="section-timestamp" value="' + formatStartTime(section.startTime) + '" />'); - to enable editing remove the disabled property HERE
+    var sectionTimestampElement = $('<div class="ui top left attached label"><input class="label-formatting" placeholder="Speaker" type="text" size="10" maxlength="16" class="section-timestamp" value="' + formatStartTime(time) + '" disabled /></div>');
+    //var sectionTimestampElement = $("<div class='ui top left attached label'>"+time+"</div>");
+    return sectionTimestampElement;
+}
+
+/**
+ * Given a section object create the word elements, add to the editor
+ * @param section a section object
+ */
+function createSectionEditorElement(values) {
+    //var sectionEditorElement = $('<div contentEditable="true" class="section-editor our-form-control"></div>');
+
+    var sectionEditorElement = $('<div class="description" contentEditable="false"></div>');
+    //var sectionEditorElement = $('<div class="description" contentEditable="false"></div>');
+    var spanElement = $('<p style="margin-top: 1%;margin-left: 2%;"></p>');
+    sectionEditorElement.append(spanElement);
+
+    firebase.database().ref("/firepads/" + transcriptKey + "/" + values.captionKey + "/history").on(
+      "child_added", function(history) {
+        //console.log("caption key " +  values.captionKey);
+        var headless = Firepad.Headless(firebase.database().ref("/firepads/" + transcriptKey + "/" + values.captionKey));
+        headless.getText(function(text) {
+          //Need to UPDATE different element here
+          spanElement.text(text);
+        });
+      });
+
+    return sectionEditorElement;
+}
+
+/**
+ * Add a section element with all the words and the editor etc. our main function
+ * @param section A real time section object
+ */
+function createSectionElement(values){
+
+    // create a section element for this section object
+    //var sectionElement = $('<div class="section"></div>');
+    var sectionElement = $('<div class="ui card"><div class="content"></div></div>');
+    //var sectionElement = $('<div class="content"></div>');
+
+    sectionElement.attr('id', values.captionKey);
+    sectionElement.attr('time', values.time);
+    sectionElement.append(createSectionHeaderElement(values));
+    sectionElement.append(createSectionEditorElement(values));
+
+    // add the listeners for adding new words
+    //addListenersForNewWord(section);
+
+    return sectionElement;
+}
+
+// Needs to be set to true as event triggers when the player first loads
+// Could be more lightweight maybe?
+function updatePlayingState(eventData) {
+    //playing = !playing;
+    console.log("play state changed");
+    state = eventData.playState;
+    if (state == "playing") {
+      playing = true;
+    }
+    else {
+      playing = false;
+    }
+}
+
+function togglePlayPause() {
+    if(playing) {
+        player.pause();
+    } else {
+        player.play();
+    }
+}
+
+
+/**
+ * Get the current player time in seconds
+ *
+ * @returns {double} SS.sssssss
+ */
+function getPlayerTime() {
+    return player.getCurrentTime();
+}
+
+/**
+* Get the length of the current video
+*
+* @returns {double} SS.sssssss
+*/
+function getDuration() {
+  return player.getDuration();
+}
+
+/**
+ * Set the player to the provided time in seconds
+ *
+ * @param time start-time in seconds
+ */
+function setPlayerTime(time) {
+    //player.currentTime = time;
+    player.seekTo(time);
+}
+
+/**
+ * Format SS.sss to HH:MM:SS, sss
+ *
+ * @param d time in seconds
+ * @returns {string} formatted time stamp string
+ */
+function secondsToHms(d) {
+    d = Number(d);
+    //console.log(d);
+
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    var ms = d - Math.floor(d);
+    ms = Math.floor(ms*1000);
+
+    h = ('00' + h).slice(-2);
+    m = ('00' + m).slice(-2);
+    s = ('00' + s).slice(-2);
+
+    return (h+':'+m+':'+s+', '+ms);
+}
+
+/**
+ * Convert a timestamp string to milliseconds
+ * @param timestamp A string containing at least HH:MM:SS, SSS
+ */
+function hmsToSeconds(timestamp) {
+    var dateRegex = /\d\d\s*:\s*\d\d\s*:\s*\d\d\s*,\s*\d\d\d/;
+    var timestamp = _.head(timestamp.match(dateRegex));
+
+    if (timestamp) {
+        var parts = timestamp.split(',');
+
+        var hms = parts[0].split(':');
+        var ms = +parts[1] / 1000;
+        var hours = +hms[0] * 3600;
+        var minutes = +hms[1] * 60;
+        var seconds = +hms[2];
+
+        return hours + minutes + seconds + ms;
+    }
+}
+
+function addDocumentListeners() {
+  document.addEventListener("keydown", function (event) {
+      if (event.keyCode == 37 && event.shiftKey) {
+          event.preventDefault();
+
+          firebase.database().ref("/captions/" + transcriptKey).orderByChild("time").endAt(getPlayerTime())
+          .limitToLast(2).once("value").then(function (snapshot) {
+            var value = snapshot.val();
+            var i = 0;
+            for (key in value) {
+              return value[key].time;
+            }
+          }).then(function(time) {
+              setPlayerTime(time);
+          });
+      }
+
+      if (event.keyCode == 39 && event.shiftKey) {
+        event.preventDefault();
+        firebase.database().ref("/captions/" + transcriptKey).orderByChild("time").startAt(getPlayerTime())
+        .limitToFirst(2).once("value").then(function(snapshot) {
+          var value = snapshot.val();
+          var numKeys = Object.keys(value).length;
+          var count = 1;
+          if(numKeys == 2) {
+            for (key in value) {
+              if(count == 2) {
+                return value[key].time;
+              }
+
+              else {
+                count++;
+              }
+            }
+          }
+
+          else {
+            for (key in value) {
+              return value[key].time;
+            }
+          }
+        }).then(function (time) {
+          setPlayerTime(time);
+        });
+      }
+
+      //
+      if (event.keyCode == 8 && event.shiftKey) {
+        event.preventDefault();
+        firebase.database().ref("/captions/" + transcriptKey).orderByChild("time").endAt(getPlayerTime()).limitToLast(1)
+        .once("value").then(function(snapshot) {
+          var value = snapshot.val();
+          for(key in value) {
+            return value[key].time;
+          }
+        }).then(function(time) {
+          setPlayerTime(time);
+        });
+      }
+
+      if(event.keyCode == 32 && event.shiftKey) {
+        event.preventDefault();
+        togglePlayPause();
+      }
+
+      //F7 rewind 10 seconds
+      if(event.keyCode === 118 && event.ctrlKey) {
+        var time = getPlayerTime();
+        var cor = time < 10? 0 : time - 10;
+        setPlayerTime(cor);
+      }
+
+      //F8 forward 10 seconds
+      if(event.keyCode === 119 && event.ctrlKey) {
+        var time = getPlayerTime();
+        var duration = getDuration();
+        var cor = duration - time < 10? duration : time + 10;
+        setPlayerTime(cor);
+      }
+
+      //F9 key held down will play
+      if(event.keyCode === 120 && !playKeyDown) {
+        playKeyDown = true;
+
+        if (!playing) {
+          togglePlayPause();
+        }
+      }
+  }, false);
+
+  document.addEventListener('keyup', function(event) {
+    //f9 key released will pause audio
+    if(event.keyCode === 120 && playKeyDown) {
+        playKeyDown = false;
+        togglePlayPause();
+    }
+  }, false);
+
+  $('.collabno').text(noUsers);
+  $('.unhide.icon')
+    .popup({
+      popup: '.special.popup'
+    });
+
+}
+
+
+function updatePlayingState(eventData) {
+    //playing = !playing;
+    state = eventData.playState;
+    if (state == "playing") {
+      playing = true;
+    }
+    else {
+      playing = false;
+    }
+}
+
+});
